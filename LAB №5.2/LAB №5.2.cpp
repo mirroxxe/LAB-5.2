529e0d40562693f12e797cc67f076477e42c8cfc
Volodya <chuchman002@gmail.com>
2023-10-19 17:58:06 +0300

    Add project files.

---
+++ LAB №5.2/LAB №5.2.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+void S(double xp, double xk, double dx, double eps);
+void A(const double x, const double eps, int& n, double& s);
+void B(const double x, const int n, double& a);
+
+int main()
+{
+    double xp, xk, dx, eps;
+
+    std::cout << "xp = "; std::cin >> xp;
+    std::cout << "xk = "; std::cin >> xk;
+    std::cout << "dx = "; std::cin >> dx;
+    std::cout << "eps = "; std::cin >> eps;
+
+    std::cout << std::fixed;
+    std::cout << "-------------------------------------------------" << std::endl;
+    std::cout << "|" << std::setw(10) << "x" << " |"
+        << std::setw(20) << "ln(1 - x)" << " |"
+        << std::setw(15) << "S" << " |"
+        << std::setw(10) << "n" << " |"
+        << std::endl;
+    std::cout << "-------------------------------------------------" << std::endl;
+
+    S(xp, xk, dx, eps);
+
+    std::cout << "-------------------------------------------------" << std::endl;
+    return 0;
+}
+
+void S(double xp, double xk, double dx, double eps)
+{
+    double x = xp;
+    int n;
+    double s = 0;
+
+    while (x <= xk)
+    {
+        A(x, eps, n, s);
+        std::cout << "|" << std::setw(10) << std::setprecision(2) << x << " |"
+            << std::setw(20) << std::setprecision(10) << -log(1 - x) << " |"
+            << std::setw(15) << std::setprecision(10) << s << " |"
+            << std::setw(10) << n << " |"
+            << std::endl;
+        x += dx;
+    }
+}
+
+void A(const double x, const double eps, int& n, double& s)
+{
+    n = 0;
+    double a = x;
+    s = a;
+
+    do
+    {
+        n++;
+        B(x, n, a);
+        s += a;
+    } while (std::abs(a) >= eps);
+}
+
+void B(const double x, const int n, double& a)
+{
+    double R = -(x * n) / (n + 1);
+    a *= -R;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../LAB №5.2/LAB №5.2.cpp"
+
+extern void S(double xp, double xk, double dx, double eps);
+extern void A(const double x, const double eps, int& n, double& s);
+extern void B(const double x, const int n, double& a);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            double xp = 0.1;
+            double xk = 0.8;
+            double dx = 0.1;
+            double eps = 0.00001;
+            int n;
+            double s;
+
+            S(xp, xk, dx, eps);
+            A(xp, eps, n, s);
+
+            Assert::AreEqual(-log(1 - xp), s, eps);
+        }
+    };
+
